4b812f253f79bc52a9e6e9083d6db0eea797f3c4
Brian M Hunt <brianmhunt@gmail.com>
2014-08-14 08:42:37 -0400

    use gulp conncet

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 //
 // Gulpfile for Airconf-2014-Knockout
 //
-var gulp = require('gulp'),
-    webserver = require('gulp-webserver');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var options = {
+  root: 'src',
+  livereload: true,
+  port: 2014
+};
 
-gulp.task('default', function() {
-  return gulp.src('.')
-    .pipe(webserver({
-      livereload: true,
-      port: 2014
-    }));
+
+
+gulp.task('connect', function() {
+  gulp.task('connect', function () {
+    connect.server(options)
+  });
 });
+
+gulp.task('src', function () {
+  gulp.src('./src/*')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/*'], ['src']);
+});
+
+gulp.task('default', ['connect', 'watch']);
